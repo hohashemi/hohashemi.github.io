9cae8c1257683b37f08a0a877fb4dbcd35dbc41f
Rick Sanchez <hossein.lordss@gmail.com>
2025-08-11 19:31:17 +0330

    newcommit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  trailingSlash: true,
+  reactStrictMode: true,
+};
 
 export default nextConfig;
